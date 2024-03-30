f70eb126fbbbce6ca606ccfac9705a507da902a9
MeisyNadiaNababan <meisynadianababan@gmail.com>
2024-03-31 00:08:09 +0700

    commit

--- Praktikum06/Sorting.java
+++ Praktikum06/Sorting.java
@@ -9,6 +9,14 @@ public class Sorting {
         return -1;
     }
 
+    public static int binarySearch(int[] arr, int key) {
+        if (arr[0] < arr[arr.length - 1]) {
+            return binarySearchAsc(arr, key);
+        } else {
+            return binarySearchDesc(arr, key);
+        }
+    }
+
     public static int binarySearchAsc(int[] arr, int key) {
         int start = 0, end = arr.length - 1;
 
@@ -23,6 +31,26 @@ public class Sorting {
                 start = mid + 1;
             } else {
                 end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static int binarySearchDesc(int[] arr, int key) {
+        int start = 0, end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == key) {
+                return mid;
+            }
+
+            if (arr[mid] < key) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
         }
 
@@ -46,6 +74,16 @@ public class Sorting {
 
         if (indexAsc != -1) {
             System.out.println("Data ditemukan pada indeks ke-" + indexAsc);
+        } else {
+            System.out.println("Data tidak ditemukan");
+        }
+
+        System.out.println("\n binarySearchDesc : ");
+        int[] sortedNilaiDesc = { 50, 40, 30, 20, 10, 5, 5 };
+        int indexDesc = binarySearchDesc(sortedNilaiDesc, 5);
+
+        if (indexDesc != -1) {
+            System.out.println("Data ditemukan pada indeks ke-" + indexDesc);
         } else {
             System.out.println("Data tidak ditemukan");
         }
